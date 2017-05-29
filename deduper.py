df5d3c98848bb3d80ede52167d4b162dd4aae71e
kageanrage <kevinjpickett@gmail.com>
2017-05-30 08:34:42 +1000

    GUI improvements: fixed bug - directory pulls from GUI removed need for 'set dir' button added colours, fonts to GUI

--- deduper.py
+++ deduper.py
@@ -3,10 +3,7 @@
 # the largest of the files found in each directory
 
 # TO DO:
-# Use what's in the entry box to set Directory variable - fix current bug
-# adjust sizing (window, entry box, buttons etc)
 # show all text on GUI instead of in command prompt / console
-# add 'Clear' button to reset Directory field
 
 
 import os
@@ -16,24 +13,34 @@ from tkinter import *
 
 Test_mode = True
 
+dir_set_by_button = False
 
-def set_directory():
+
+def enable_dir_set_by_button():
+    global dir_set_by_button
+    dir_set_by_button = True
+
+
+def tell_me_which_directory():
     if len(sys.argv) > 1:
-        dir = str(sys.argv[1])  # takes the desired directory from the command line argument, passed by the batch file
+        direc = str(sys.argv[1])  # takes the desired directory from the command line argument, passed by the batch file
     else:
         if os.path.exists(r"C:\Program Files\StableBit\DrivePool"):
             print("Kev's Home PC detected, setting default test directory accordingly\n")
-            dir = r"C:\Github local repos\deduper\test"   # note this this the hardcoded directory for when working on Home PC
+            direc = r"C:\Github local repos\deduper\test"   # note this this the hardcoded directory for when working on Home PC
         else:
             print("This isn't Kev's Home PC, must be laptop, so setting test default directory accordingly\n")
-            dir = r"C:\KP Python\deduper\test"
-    return dir
+            direc = r"C:\KP Python\deduper\test"
+    if dir_set_by_button:
+        direc = get_dir_from_gui()
+    return direc
 
 
-def set_dir_from_gui():    # this isn't working because returning newdir isn't enough - I need to change it
+def get_dir_from_gui():
     new_dir = entryDir.get()
     print('Attempting to set new dir')
     return new_dir
+
 
 def get_video_filename(fname, folname, abspath):
     movie_extensions = ['.mov', '.mp4', '.mkv', '.avi']
@@ -45,6 +52,7 @@ def get_video_filename(fname, folname, abspath):
 
 
 def main_fn():
+    directory = tell_me_which_directory()
     print('Directory = {}'.format(directory))
     os.chdir(directory) # change cwd to the desired directory
     abspath = os.path.abspath('.')  # define abspath
@@ -73,39 +81,41 @@ def main_fn():
                         print('Pretending to delete file: {}'.format(fname_sorted[i]))  # INSERT CODE TO DELETE FILE
                 else:
                     continue
+    while True:
+        q = input('Press Q to quit\n')
+        q = q.upper()
+        if q == 'Q':
+            sys.exit()
 
+
+def main_triggered_from_button():
+    enable_dir_set_by_button()
+    main_fn()
 
 print('Hello, this script will crawl through the directory and sub-dirs, notify if multiple video files found in one '
       'dir, and prompt to delete each of them, excluding the largest one\n\n\n')
 
-directory = set_directory()
+directory = tell_me_which_directory()
 dir_text = str(directory)   # converts directory path to text for use by tkinter 'entryDir' as default text
 
 # TKINTER SECTION
 root = Tk()
 root.title('Deduper')
 
-labelHeading = Label(root, text='Deduper').grid(row=0, column=2)
-labelDescription = Label(root, text='Deletes all but the largest video file in each sub-dir').grid(row=1, column=2)
+labelHeading = Label(root, text='Deduper', font = 'Arial 24 bold', fg='blue', bg='gray').grid(row=0, column=2)
+labelDescription = Label(root, text='Deletes all but the largest video file in each sub-dir', font='Arial 16', fg='blue', bg='gray').grid(row=1, column=2)
 
-labelDir = Label(root, text='Directory').grid(row=2, column=1)
+labelDir = Label(root, text='Directory', font='Arial 16', fg='blue', bg='gray').grid(row=2, column=1)
 entryDir = Entry(root, width=50)
 entryDir.insert(10, dir_text)
 entryDir.grid(row=2,column=2)
 
 
-quitButton = Button(root,text='Quit',command=quit).grid(row=4,column=1)
-setDirButton = Button(root, text='Set Dir', command=set_dir_from_gui).grid(row=4, column=2)
-goButton = Button(root,text='Go',command=main_fn).grid(row=4,column=3)
+quitButton = Button(root,text='Quit', font='Arial 16',command=quit, fg='red', bg='gray').grid(row=4,column=1)
+goButton = Button(root,text='Go', font='Arial 24 bold',command=main_triggered_from_button, fg='green', bg='gray').grid(row=4,column=3)
 mainloop()
-
-
-# main_fn()   # COMMENTED OUT AS CURRENTLY TRIGGERED BY 'Go' BUTTON
 
 
 # PRESS Q TO QUIT
 
-q = input('Press Q to quit\n')
-q = q.upper()
-if q == 'Q':
-    sys.exit()
+
