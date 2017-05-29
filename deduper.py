0727e1438e565847aa25ca36974e5d219d8b44fb
kageanrage <kevinjpickett@gmail.com>
2017-05-29 17:27:21 +1000

    added basic partly-functioning GUI using Tkinter

--- deduper.py
+++ deduper.py
@@ -2,13 +2,20 @@
 # Deduper.py - Script to crawl through Movies directory, notify if multiple video files and prompt to delete all but
 # the largest of the files found in each directory
 
+# TO DO:
+# Use what's in the entry box to set Directory variable
+# adjust sizing (window, entry box, buttons etc)
+# show all text on GUI instead of in command prompt / console
+# add 'Clear' button to reset Directory field
+
 
 import os
 import send2trash
 import sys
+from tkinter import *
 
+Test_mode = True
 
-Test_mode = False
 
 def set_directory():
     if len(sys.argv) > 1:
@@ -23,7 +30,7 @@ def set_directory():
     return dir
 
 
-def get_video_filename(fname, folname):
+def get_video_filename(fname, folname, abspath):
     movie_extensions = ['.mov', '.mp4', '.mkv', '.avi']
     basename, ext = os.path.splitext(fname) # isolate basename and extensions
     if ext in movie_extensions:
@@ -31,37 +38,58 @@ def get_video_filename(fname, folname):
         fname_with_path = os.path.join(abspath, folname, fname)    # new name incl path
         return fname_with_path
 
-
-directory = set_directory()
-os.chdir(directory) # change cwd to the desired directory
-abspath = os.path.abspath('.')  # define abspath
+def main_fn():
+    directory = set_directory()
+    os.chdir(directory) # change cwd to the desired directory
+    abspath = os.path.abspath('.')  # define abspath
+    for folderName, subfolders, filenames in os.walk(directory):
+        movie_files_detected = []
+        for filename in filenames:
+            if get_video_filename(filename, folderName, abspath) != None:
+                movie_files_detected.append(get_video_filename(filename, folderName, abspath))
+        fname_sorted = sorted(movie_files_detected, key=os.path.getsize, reverse=True)
+        if len(fname_sorted) > 1:
+            print('Multiple video files detected in folder {}'.format(folderName))
+            print("The largest video file in the folder, which we won't touch, is {}\n".format(fname_sorted[0]))
+            print('Potential files to delete are as follows:')
+            for i in range(1, len(fname_sorted)):
+                print('File to delete is {}, size is {}'.format(fname_sorted[i], os.path.getsize(fname_sorted[i])))
+            for i in range(1, len(fname_sorted)):
+                response = input('\nPress Y to delete or N to ignore: {}, size is {}\n'.format(fname_sorted[i],
+                                                                                               os.path.getsize(
+                                                                                                   fname_sorted[i])))
+                response = response.upper()
+                if response == 'Y':
+                    if not Test_mode:
+                        print('Sending to Recycle Bin: {}'.format(fname_sorted[i]))
+                        send2trash.send2trash(fname_sorted[i])
+                    else:
+                        print('Pretending to delete file: {}'.format(fname_sorted[i]))  # INSERT CODE TO DELETE FILE
+                else:
+                    continue
 
 
 print('Hello, this script will crawl through the directory and sub-dirs, notify if multiple video files found in one '
       'dir, and prompt to delete each of them, excluding the largest one\n\n\n')
-for folderName, subfolders, filenames in os.walk(directory):
-    movie_files_detected = []
-    for filename in filenames:
-        if get_video_filename(filename, folderName) != None:
-            movie_files_detected.append(get_video_filename(filename, folderName))
-    fname_sorted = sorted(movie_files_detected, key=os.path.getsize, reverse=True)
-    if len(fname_sorted) > 1:
-        print('Multiple video files detected in folder {}'.format(folderName))
-        print("The largest video file in the folder, which we won't touch, is {}\n".format(fname_sorted[0]))
-        print('Potential files to delete are as follows:')
-        for i in range(1, len(fname_sorted)):
-            print('File to delete is {}, size is {}'.format(fname_sorted[i], os.path.getsize(fname_sorted[i])))
-        for i in range(1, len(fname_sorted)):
-            response = input('\nPress Y to delete or N to ignore: {}, size is {}\n'.format(fname_sorted[i], os.path.getsize(fname_sorted[i])))
-            response = response.upper()
-            if response == 'Y':
-                if not Test_mode:
-                    print('Sending to Recycle Bin: {}'.format(fname_sorted[i]))
-                    send2trash.send2trash(fname_sorted[i])
-                else:
-                    print('Pretending to delete file: {}'.format(fname_sorted[i])) # INSERT CODE TO DELETE FILE
-            else:
-                continue
+
+# TKINTER SECTION
+root = Tk()
+root.title('Deduper')
+
+labelFirst = Label(root,text='Directory').grid(row=2,column=1)
+firstEntry = Entry(root)
+firstEntry.insert(10, r"C:\KP Python\deduper\test")
+firstEntry.grid(row=2,column=5)
+
+quitButton = Button(root,text='Quit',command=quit).grid(row=4,column=1)
+goButton = Button(root,text='Go',command=main_fn).grid(row=4,column=2)
+mainloop()
+
+
+# main_fn()   # COMMENTED OUT AS CURRENTLY TRIGGERED BY 'Go' BUTTON
+
+
+# PRESS Q TO QUIT
 
 q = input('Press Q to quit\n')
 q = q.upper()
